0ba44d25e1826d7b58ba591515e15aebebd20c0d
sushil-oss <sharma.sushil265@gmail.com>
2025-12-27 18:04:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-gh"
   location = "West Europe"
 }
-
+terraform {
  required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.52.0"
     }
   }
+}
 
   provider "azurerm" {
   features {}
-  subscription_id = "345f5a23-b761-4ede-ab2b-beedb9d58ab1"
+  subscription_id = "6c750392-d85d-41e6-a2be-4dc4ddc9d11d"
 }
 
